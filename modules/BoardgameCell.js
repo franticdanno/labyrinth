ef3f55be6b557cfba884e442e365e987602d5866
Daniel O'Regan <franticdanno@gmail.com>
2020-05-03 12:50:04 +0100

    Fixing issue with identifying links rotation

--- modules/BoardgameCell.js
+++ modules/BoardgameCell.js
@@ -73,7 +73,8 @@ export default class BoardGameCell extends PIXI.Container {
 
   GetLinks = () => {
     let og = CELL_TYPE_LINKS[this.celltype]
-    let currentRotation = ((this.rotation * (180/Math.PI)) / 90) % 4;
+    let currentRotation = Math.floor(((this.rotation * (180/Math.PI)) / 90) % 4)
+    console.log("Current rotation is",currentRotation)
     //console.log("Getting Links",this.rotation,og,currentRotation);
     switch (currentRotation){
       case 0:
