6b42c12cbbfd814c8872a4bc9258231974f29530
Daniel O'Regan <franticdanno@gmail.com>
2020-05-11 09:02:49 +0100

    Adding more tweens

--- modules/libs/tween/Tween.js
+++ modules/libs/tween/Tween.js
@@ -125,3 +125,49 @@ Tween.easeInOutCirc = function (t, b, c, d) {
 	t -= 2;
 	return c/2 * (Math.sqrt(1 - t*t) + 1) + b;
 };
+
+Tween.easeInBounce = function (t, b, c, d) {
+		return c - Tween.easeOutBounce (d-t, 0, c, d) + b;
+};
+
+Tween.easeOutBounce = function (t, b, c, d) {
+  if ((t/=d) < (1/2.75)) {
+    return c*(7.5625*t*t) + b;
+  } else if (t < (2/2.75)) {
+    return c*(7.5625*(t-=(1.5/2.75))*t + .75) + b;
+  } else if (t < (2.5/2.75)) {
+    return c*(7.5625*(t-=(2.25/2.75))*t + .9375) + b;
+  } else {
+    return c*(7.5625*(t-=(2.625/2.75))*t + .984375) + b;
+  }
+};
+
+Tween.easeInOutBounce = function (t, b, c, d) {
+  if (t < d/2) return Tween.easeInBounce (t*2, 0, c, d) * .5 + b;
+  return Tween.easeOutBounce (t*2-d, 0, c, d) * .5 + c*.5 + b;
+};
+
+Tween.easeInElastic = function (t, b, c, d) {
+		var s=1.70158;var p=0;var a=c;
+		if (t==0) return b;  if ((t/=d)==1) return b+c;  if (!p) p=d*.3;
+		if (a < Math.abs(c)) { a=c; var s=p/4; }
+		else var s = p/(2*Math.PI) * Math.asin (c/a);
+		return -(a*Math.pow(2,10*(t-=1)) * Math.sin( (t*d-s)*(2*Math.PI)/p )) + b;
+	};
+
+Tween.easeOutElastic = function (t, b, c, d) {
+		var s=1.70158;var p=0;var a=c;
+		if (t==0) return b;  if ((t/=d)==1) return b+c;  if (!p) p=d*.3;
+		if (a < Math.abs(c)) { a=c; var s=p/4; }
+		else var s = p/(2*Math.PI) * Math.asin (c/a);
+		return a*Math.pow(2,-10*t) * Math.sin( (t*d-s)*(2*Math.PI)/p ) + c + b;
+	};
+
+Tween.easeInOutElastic = function (t, b, c, d) {
+		var s=1.70158;var p=0;var a=c;
+		if (t==0) return b;  if ((t/=d/2)==2) return b+c;  if (!p) p=d*(.3*1.5);
+		if (a < Math.abs(c)) { a=c; var s=p/4; }
+		else var s = p/(2*Math.PI) * Math.asin (c/a);
+		if (t < 1) return -.5*(a*Math.pow(2,10*(t-=1)) * Math.sin( (t*d-s)*(2*Math.PI)/p )) + b;
+		return a*Math.pow(2,-10*(t-=1)) * Math.sin( (t*d-s)*(2*Math.PI)/p )*.5 + c + b;
+	};
